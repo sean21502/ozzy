2cd429768d523ab3f940fd5b62b598fb4ded79e9
Sean Hite <seanhite@gmail.com>
2017-11-24 20:14:46 -0500

    curl error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :applications
+	namespace :api, defaults: { format: :json } do
+
+  match '/events', to: 'events#preflight', via: [:options]
+
+  resources :applications, :events, only: [:create]
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
